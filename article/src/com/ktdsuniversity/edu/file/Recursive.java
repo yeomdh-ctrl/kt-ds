630a5c86f706a9c17db95db5076b413ce2d1b03d
yeomdh-ctrl <yeomdh0302@gmail.com>
2026-02-03 17:00:42 +0900

    재귀호출

--- article/src/com/ktdsuniversity/edu/file/Recursive.java
+++ article/src/com/ktdsuniversity/edu/file/Recursive.java
@@ -1,6 +1,14 @@
 package com.ktdsuniversity.edu.file;
 
 public class Recursive {
+	public int sumToZero(int start) {
+		// start ==> 5
+		// > start == 1
+		if(start == 1) {
+			return 1;
+		}
+		return start + sumToZero(start - 1);
+	}
 	
 	public void printNumber(int number) {
 		System.out.println(number);
@@ -9,18 +17,21 @@ public class Recursive {
 		}
 	}
 	
-//	public void print(int number) {
-//		System.out.println("출력합니다. " + number);
-//		if(number < 3) {
-//		print(number + 1);
-//		System.out.println((number) + "스택 실행을 완료했습니다");
-//		}
-//	}
+	public void print(int number) {
+		System.out.println("출력합니다. " + number);
+		if(number < 3) {
+		print(number + 1);
+		System.out.println((number) + "스택 실행을 완료했습니다");
+		}
+	}
+	
 
 	public static void main(String[] args) {
 		Recursive r = new Recursive();
 //		r.print(1);
-		r.printNumber(2000); // 결과 ==> 2000~0
+//		r.printNumber(2000); // 결과 ==> 2000~0
+		int result = r.sumToZero(5);
+		System.out.println(result);
 	}
 
 }
